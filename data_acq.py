bb39dcff62c879504f75197c4b2cb355c100df17
Bhaumik Shah - C15636 <C15636@mchp-main.com>
2016-06-06 16:32:19 -0700

    inital commit

---
+++ data_acq.py
@@ -0,0 +1,29 @@
+# Coordinate class to get coordinates from google MAP API
+# Output will be fed to forecast.io api for weather information
+
+import requests
+
+
+class Coordinates:
+    # Initializing coordinate class
+    def __init__(self, address_line1, city, state):
+        self.address = address_line1
+        self.city = city
+        self.state = state
+        self.lat = 0
+        self.lng = 0
+
+    # using request for python getting lat and lng for the given address
+    def get_geocode(self):
+        payload = {'address': self.address + self.city + self.state, 'key': ''}
+        r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?', params=payload)
+        print (r.url)
+        geo_code = r.json()
+        self.lat = geo_code[u'results'][0][u'geometry'][u'location'][u'lat']
+        self.lng = geo_code[u'results'][0][u'geometry'][u'location'][u'lng']
+        print self.lat, self.lng
+
+
+
+
+
